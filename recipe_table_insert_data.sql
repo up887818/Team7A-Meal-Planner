a06418940800d556d26e8614867c97dcf37c9fca
GerardGannon <60616870+GerardGannon@users.noreply.github.com>
2020-03-08 13:32:52 +0000

    Add files via upload

---
+++ recipe_table_insert_data.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO recipe
+VALUES (DEFAULT,'Chicken Pasta Bake','30 mins to 1 hour','532 kcal','22g','35g','0.9g','10g','4.5g','46g');
+INSERT INTO recipe
+VALUES (DEFAULT,'Vegetable Stir Fry','10 to 30 mins','103 kcal','6g','3g','1.4g','0.7g','3.5g','7g');
+INSERT INTO recipe
+VALUES (DEFAULT,'Chicken Curry','10 to 30 mins','164 kcal','2.9g','21g','1.4g','11g','4.1g','13.7g');
+INSERT INTO recipe
+VALUES (DEFAULT,'Vegan Chilli','30 mins to 1 hour','501 kcal','16g','18g','1.4g','18g','17g','71g');
+INSERT INTO recipe
+VALUES (DEFAULT,'Shepherd's pie','1 to 2 hours','743 kcal','46g','34g','1.2g','10g','5.5g','45g');
