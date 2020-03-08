b9ccac1abbdc411f5513cab8441e67704a967de5
GerardGannon <60616870+GerardGannon@users.noreply.github.com>
2020-03-08 12:43:56 +0000

    Update meal_app_queries.sql

--- meal_app_queries.sql
+++ meal_app_queries.sql
@@ -14,3 +14,11 @@ join recipe_allergen on recipe.recipe_id = recipe_allergen.recipe_id
 join allergen on recipe_allergen.allergen_id = allergen.allergen_id
 where recipe_id = 1;
 --the id '1' should be an input
+
+
+--filter by cooking time
+select recipe_name, cooking_time, cuisine_name
+from recipe
+join recipe_cuisine on recipe.recipe_id = recipe_cuisine.recipe_id
+join cuisine on recipe_cuisine.cuisine_id = cuisine.cuisine_id;
+where cooking_time BETWEEN '10' AND '25';
