8620144287c1c272da1a51e79da671e6baf46645
up900921 <60406174+up900921@users.noreply.github.com>
2020-03-12 14:58:56 +0000

    Updated create table statements
    All calories data will be stored as int (so in the kcal). Also updated user table, so it can store user's preferences like calories or cuisine, and allergies.

--- mealprep_database.sql
+++ mealprep_database.sql
@@ -4,14 +4,14 @@ create database mealprep;
 
 create table cuisine (
     cuisine_id serial primary key,
-    cuisine_name varchar(20)
+    cuisine_name varchar(20) not null
 );
 
 create table recipe (
     recipe_id serial primary key,
-    recipe_name varchar(25),
+    recipe_name varchar(25) not null,
     cooking_time varchar(10),
-    calories varchar(20),
+    calories int,
     fat varchar (20),
     protein varchar (20),
     salt varchar (20),
@@ -22,7 +22,7 @@ create table recipe (
 
 create table allergen (
     allergen_id serial primary key,
-    allergen_name varchar(20)
+    allergen_name varchar(20) not null
 );
 
 /* link table between recipe table and cuisine table to resolve many-to-many relationship */
@@ -42,9 +42,14 @@ create table recipe_allergen (
 );
 
 
-create table user ?
+create table user (
     user_id serial primary key,
-    first_name varchar(50),
+    first_name varchar(50) not null,
     last_name varchar(50),
-    email varchar(50)
+    email varchar(50) not null,
+    calories int,
+    allergen_id int,
+    cuisine_id int,
+    foreign key (allergen_id) references allergen(allergen_id),
+    foreign key (cuisine_id) references cuisine(cuisine_id)
 );
