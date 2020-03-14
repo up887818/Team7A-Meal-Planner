b6a9fa71c6f49639bb2a5e5ae31e91670447ecaf
up887818 <57417766+up887818@users.noreply.github.com>
2020-03-14 16:19:40 +0000

    Update server.js

--- server.js
+++ server.js
@@ -72,11 +72,32 @@ async function runTests() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  //error catching
+  page.on('requestfailed', request => {
+    console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`)
+  });
+  // Catch console log errors
+  page.on("pageerror", err => {
+    console.log(`Page error: ${err.toString()}`);
+  });
+  // Catch all console messages
+  page.on('console', msg => {
+    console.log('Logger:', msg.type());
+    console.log('Logger:', msg.text());
+    console.log('Logger:', msg.location());
+
+  });
+
   await page.goto('localhost:8080/');
-  //homepage
+  await page.waitFor(1000);
+  //homepage, waits 1000ms to make sure its loaded
 
   await page.goto('localhost:8080/showAll');
+  await page.waitFor(1000);
   //show all recipes
+
+  await browser.close();
+  //close browser
 }
 
 app.get('/recipe', getRecipe);
