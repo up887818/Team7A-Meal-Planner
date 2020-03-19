3b55a5d21734b3f38364c1395381ffc5f7209555
up900921 <60406174+up900921@users.noreply.github.com>
2020-03-19 22:25:18 +0000

    Update mock data

--- client/database/mock_data.sql
+++ client/database/mock_data.sql
@@ -1,29 +1,16 @@
---table cuisine
-insert into cuisine (cuisine_id, cuisine_name)
-  values
-  (1, 'japanese'),
-  (2, 'indian'),
-  (3, 'mexican'),
-  (4, 'thai'),
-  (5, 'greek'),
-  (6, 'american'),
-  (7, 'british'),
-  (8, 'french'),
-  (9, 'portugese');
-
 --table recipe
-insert into recipe (recipe_id, recipe_name, cooking_time, calories, fat, protein, salt, sugar, fibre, carbonhydrates)
+insert into recipe (recipe_id, recipe_name, cooking_time, cuisine, calories, fat, protein, salt, sugar, fibre, carbonhydrates)
   values
-  (1, 'lobortis sapien', '0:30', 973, 40, 47, 33, 72, 96, 49),
-  (2, 'nec', '0:15', 316, 56, 43, 49, 63, 2, 31),
-  (3, 'quam sapien', '1:15', 553, 85, 79, 97, 83, 34, 63),
-  (4, 'libero', '1:00', 425, 39, 84, 58, 68, 61, 38),
-  (5, 'curae donec', '0:40', 716, 58, 94, 58, 67, 9, 40),
-  (6, 'amet', '0:15', 665, 2, 75, 34, 24, 14, 99),
-  (7, 'quam', '0:30', 482, 73, 18, 79, 16, 81, 26),
-  (8, 'leo rhoncus', '0:50', 225, 84, 75, 7, 25, 82, 92),
-  (9, 'ultrices enim', '0:25', 424, 78, 59, 30, 43, 79, 5),
-  (10, 'id', '0:35', 255, 39, 18, 11, 94, 97, 4);
+  (1, 'lobortis sapien', '0:30', 'french', 973, 40, 47, 33, 72, 96, 49),
+  (2, 'nec', '0:15', 'japanese', 316, 56, 43, 49, 63, 2, 31),
+  (3, 'quam sapien', '1:15', 'indian', 553, 85, 79, 97, 83, 34, 63),
+  (4, 'libero', '1:00', 'british', 425, 39, 84, 58, 68, 61, 38),
+  (5, 'curae donec', '0:40', 'portugese', 716, 58, 94, 58, 67, 9, 40),
+  (6, 'amet', '0:15', 'indian', 665, 2, 75, 34, 24, 14, 99),
+  (7, 'quam', '0:30', 'japanese', 482, 73, 18, 79, 16, 81, 26),
+  (8, 'leo rhoncus', '0:50', 'british', 225, 84, 75, 7, 25, 82, 92),
+  (9, 'ultrices enim', '0:25', 'indian', 424, 78, 59, 30, 43, 79, 5),
+  (10, 'id', '0:35', 'french', 255, 39, 18, 11, 94, 97, 4);
 
 --table allergen
 insert into allergen (allergen_id, allergen_name)
@@ -41,20 +28,6 @@ insert into allergen (allergen_id, allergen_name)
   (11, 'lupin'),
   (12, 'crustaceans'),
   (13, 'gluten');
-
---table recipe_cuisine
-insert into recipe_cuisine (recipe_id, cuisine_id)
-  values
-  (4, 9),
-  (6, 3),
-  (3, 7),
-  (3, 4),
-  (3, 3),
-  (10, 6),
-  (7, 8),
-  (5, 5),
-  (8, 7),
-  (8, 1);
 
 --table recipe_allergen
 insert into recipe_allergen (recipe_id, allergen_id)
