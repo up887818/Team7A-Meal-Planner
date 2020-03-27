4572c207b14371532eaad477190f5553384f42bc
TruworthsNyesah <60390226+TruworthsNyesah@users.noreply.github.com>
2020-03-27 12:53:31 +0000

    added recipe names

--- client/database/mock_data.sql
+++ client/database/mock_data.sql
@@ -3,16 +3,16 @@ create extension if not exists pgcrypto;
 --table recipe
 insert into recipe (recipe_id, recipe_name, cooking_time, cuisine, calories, fat, protein, salt, sugar, fibre, carbonhydrates)
   values
-  (1, 'lobortis sapien', '0:30', 'french', 973, 40, 47, 33, 72, 96, 49),
-  (2, 'nec', '0:15', 'japanese', 316, 56, 43, 49, 63, 2, 31),
-  (3, 'quam sapien', '1:15', 'indian', 553, 85, 79, 97, 83, 34, 63),
-  (4, 'libero', '1:00', 'british', 425, 39, 84, 58, 68, 61, 38),
-  (5, 'curae donec', '0:40', 'portugese', 716, 58, 94, 58, 67, 9, 40),
-  (6, 'amet', '0:15', 'indian', 665, 2, 75, 34, 24, 14, 99),
-  (7, 'quam', '0:30', 'japanese', 482, 73, 18, 79, 16, 81, 26),
-  (8, 'leo rhoncus', '0:50', 'british', 225, 84, 75, 7, 25, 82, 92),
-  (9, 'ultrices enim', '0:25', 'indian', 424, 78, 59, 30, 43, 79, 5),
-  (10, 'id', '0:35', 'french', 255, 39, 18, 11, 94, 97, 4);
+  (1, 'Fish soup', '0:30', 'french', 973, 40, 47, 33, 72, 96, 49),
+  (2, 'Teriyaki Salmon', '0:15', 'japanese', 316, 56, 43, 49, 63, 2, 31),
+  (3, 'Lamb curry', '1:15', 'indian', 553, 85, 79, 97, 83, 34, 63),
+  (4, 'Toad in a hole', '1:00', 'british', 425, 39, 84, 58, 68, 61, 38),
+  (5, 'Chicken piri-piri', '0:40', 'portugese', 716, 58, 94, 58, 67, 9, 40),
+  (6, 'Chickpeas and poached eggs', '0:15', 'indian', 665, 2, 75, 34, 24, 14, 99),
+  (7, 'Ramen noodles', '0:30', 'japanese', 482, 73, 18, 79, 16, 81, 26),
+  (8, 'Scotch eggs', '0:50', 'british', 225, 84, 75, 7, 25, 82, 92),
+  (9, 'Bread fritters', '0:25', 'indian', 424, 78, 59, 30, 43, 79, 5),
+  (10, 'Steak hache with pomme frites', '0:35', 'french', 255, 39, 18, 11, 94, 97, 4);
 
 --table allergen
 insert into allergen (allergen_id, allergen_name)
